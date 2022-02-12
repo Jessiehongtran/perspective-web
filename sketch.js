5bc0fb0d2e26075fd72b107a63985121bd5c21a3
Hong Tran <jessietran@fb.com>
2022-02-11 23:46:39 -0800

    update

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var img;
 
 function preload(){
     img = loadImage('https://res.cloudinary.com/dfulxq7so/image/upload/v1610488276/CoolMap_irfdae.png')
-    createCanvas(720, 400);
+    createCanvas(1000, 1000);
 }
 
 function setup(){
